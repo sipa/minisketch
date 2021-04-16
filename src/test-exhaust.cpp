15649178340ba7f77a8d3b1905deb502f5258389
Pieter Wuille <pieter@wuille.net>
2021-04-15 19:00:10 -0700

    Rename count -> capacity in tests

--- src/test-exhaust.cpp
+++ src/test-exhaust.cpp
@@ -23,15 +23,15 @@ uint64_t Combination(uint64_t n, uint64_t k) {
     return ret;
 }
 
-std::vector<uint64_t> TestAll(int bits, int impl, int count) {
+std::vector<uint64_t> TestAll(int bits, int impl, int capacity) {
     bool supported = minisketch_implementation_supported(bits, impl);
-    minisketch* state = minisketch_create(bits, impl, count);
+    minisketch* state = minisketch_create(bits, impl, capacity);
     CHECK(supported == (state != nullptr));
     std::vector<uint64_t> ret;
     if (!state) return ret;
 
-    // Iterate over all (bits)-bit sketches with (count) syndromes.
-    for (uint64_t x = 0; (x >> (bits * count)) == 0; ++x) {
+    // Iterate over all (bits)-bit sketches with (capacity) syndromes.
+    for (uint64_t x = 0; (x >> (bits * capacity)) == 0; ++x) {
         // Construct the serialization and load it.
         unsigned char ser[8];
         ser[0] = x;
@@ -54,7 +54,7 @@ std::vector<uint64_t> TestAll(int bits, int impl, int count) {
             // Asking for one root less should fail.
             CHECK(num_roots < 1 || minisketch_decode(state, num_roots - 1, roots) == -1);
             // Reconstruct the sketch from the solutions.
-            minisketch* state2 = minisketch_create(bits, 0, count);
+            minisketch* state2 = minisketch_create(bits, 0, capacity);
             for (int i = 0; i < num_roots; ++i) {
                 minisketch_add_uint64(state2, roots[i]);
             }
@@ -74,38 +74,38 @@ std::vector<uint64_t> TestAll(int bits, int impl, int count) {
     }
     minisketch_destroy(state);
 
-    for (int i = 1; i <= count + 1; ++i) {
+    for (int i = 1; i <= capacity + 1; ++i) {
         CHECK(ret[i] == Combination((uint64_t(1) << bits) - 1, i - 1));
     }
     return ret;
 }
 
-void TestRand(int bits, int impl, int count, int iter) {
-    std::vector<uint64_t> elems(count);
-    std::vector<uint64_t> roots(count + 1);
+void TestRand(int bits, int impl, int capacity, int iter) {
+    std::vector<uint64_t> elems(capacity);
+    std::vector<uint64_t> roots(capacity + 1);
     std::random_device rnd;
     std::uniform_int_distribution<uint64_t> dist(1, bits == 64 ? -1 : ((uint64_t(1) << bits) - 1));
 
     for (int i = 0; i < iter; ++i) {
         bool overfill = iter & 1; // Test some cases with overfull sketches that may not decode.
-        minisketch* state = minisketch_create(bits, impl, count);
+        minisketch* state = minisketch_create(bits, impl, capacity);
         if (!state) return;
-        minisketch* basestate = minisketch_create(bits, 0, count);
-        for (int j = 0; j < count + 3*overfill; ++j) {
+        minisketch* basestate = minisketch_create(bits, 0, capacity);
+        for (int j = 0; j < capacity + 3*overfill; ++j) {
             uint64_t r = dist(rnd);
             if (!overfill) elems[j] = r;
             minisketch_add_uint64(state, r);
             minisketch_add_uint64(basestate, r);
         }
-        roots.assign(count + 1, 0);
+        roots.assign(capacity + 1, 0);
         std::vector<unsigned char> data, basedata;
-        basedata.resize(((count + 1) * bits + 7) / 8);
-        data.resize(((count + 1) * bits + 7) / 8);
+        basedata.resize(((capacity + 1) * bits + 7) / 8);
+        data.resize(((capacity + 1) * bits + 7) / 8);
         minisketch_serialize(basestate, basedata.data());
         minisketch_serialize(state, data.data());
         CHECK(data == basedata);
         minisketch_deserialize(state, basedata.data());
-        int num_roots = minisketch_decode(state, count + 1, roots.data());
+        int num_roots = minisketch_decode(state, capacity + 1, roots.data());
         CHECK(overfill || num_roots >= 0);
         CHECK(num_roots < 1 || minisketch_decode(state, num_roots - 1, roots.data()) == -1); // Decoding with a too-low maximum should fail.
         if (!overfill) {
@@ -193,34 +193,34 @@ int main(void) {
         fprintf(stderr, "%i random tests with %i bits: done\n", 500 / j, j);
     }
 
-    int counts[65] = {0};
+    int capacities[65] = {0};
     // Initialize capacities to 1 because a 0 capacity is not allowed.
     for (int bits = 0; bits < 65; ++bits) {
-        counts[bits] = 1;
+        capacities[bits] = 1;
     }
     for (int weight = 0; weight <= 40; weight += 1) {
         for (int bits = 2; bits <= 32; ++bits) {
-            int count = counts[bits];
-            while (count < (1 << bits) && count * bits <= weight) {
-                auto ret = TestAll(bits, 0, count);
-                auto ret2 = TestAll(bits, 1, count);
-                auto ret3 = TestAll(bits, 2, count);
+            int capacity = capacities[bits];
+            while (capacity < (1 << bits) && capacity * bits <= weight) {
+                auto ret = TestAll(bits, 0, capacity);
+                auto ret2 = TestAll(bits, 1, capacity);
+                auto ret3 = TestAll(bits, 2, capacity);
                 CHECK(ret2.empty() || ret == ret2);
                 CHECK(ret3.empty() || ret == ret3);
-                fprintf(stderr, "bits=%i count=%i below_bound=[", bits, count);
-                for (int i = 0; i <= count; ++i) {
+                fprintf(stderr, "bits=%i capacity=%i below_bound=[", bits, capacity);
+                for (int i = 0; i <= capacity; ++i) {
                     if (i) fprintf(stderr,  ",");
                     fprintf(stderr, "%llu", (unsigned long long)ret[i + 1]);
                 }
                 fprintf(stderr, "] above_bound=[");
-                for (int i = count + 1; i + 1 < (int)ret.size(); ++i) {
-                    if (i > count + 1) fprintf(stderr,  ",");
+                for (int i = capacity + 1; i + 1 < (int)ret.size(); ++i) {
+                    if (i > capacity + 1) fprintf(stderr,  ",");
                     fprintf(stderr, "%llu/%llu", (unsigned long long)ret[i + 1], (unsigned long long)Combination((uint64_t(1) << bits) - 1, i));
                 }
-                fprintf(stderr, "] nodecode=[%g]\n", (double)ret[0] * pow(0.5, bits * count));
-                ++count;
+                fprintf(stderr, "] nodecode=[%g]\n", (double)ret[0] * pow(0.5, bits * capacity));
+                ++capacity;
             }
-            counts[bits] = count;
+            capacities[bits] = capacity;
         }
     }
 
